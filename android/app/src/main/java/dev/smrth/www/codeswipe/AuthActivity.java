f43fc0e932639710d570d10db8e452536b93bc87
immplouis <matthew.portlouis@gmail.com>
2021-10-16 23:57:38 -0500

    Finished Basic Login Screen - no reauth, sign out, profile info stuff yet

---
+++ android/app/src/main/java/dev/smrth/www/codeswipe/AuthActivity.java
@@ -0,0 +1,115 @@
+package dev.smrth.www.codeswipe;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.OAuthProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AuthActivity extends AppCompatActivity {
+
+    EditText inputEmail;
+    Button btnLogin;
+    FirebaseAuth mAuth;
+    FirebaseUser fUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        inputEmail = findViewById(R.id.emailEditText);
+        btnLogin = findViewById(R.id.signInButton);
+        mAuth = FirebaseAuth.getInstance();
+        fUser = mAuth.getCurrentUser();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = inputEmail.getText().toString();
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(AuthActivity.this, "Enter an email", Toast.LENGTH_SHORT).show();
+                }else{
+                    OAuthProvider.Builder provider = OAuthProvider.newBuilder("github.com");
+                    provider.addCustomParameter("login", email);
+
+                    List<String> scopes =
+                            new ArrayList<String>() {
+                                {
+                                    add("user:email");
+                                }
+                            };
+                    provider.setScopes(scopes);
+
+
+                    Task<AuthResult> pendingResultTask = mAuth.getPendingAuthResult();
+                    if (pendingResultTask != null) {
+                        // There's something already here! Finish the sign-in for your user.
+                        pendingResultTask
+                                .addOnSuccessListener(
+                                        new OnSuccessListener<AuthResult>() {
+                                            @Override
+                                            public void onSuccess(AuthResult authResult) {
+                                                // User is signed in.
+                                                // IdP data available in
+                                                // authResult.getAdditionalUserInfo().getProfile().
+                                                // The OAuth access token can also be retrieved:
+                                                // authResult.getCredential().getAccessToken().
+                                            }
+                                        })
+                                .addOnFailureListener(
+                                        new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Toast.makeText(AuthActivity.this,""+e.getMessage(),Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                    } else {
+                        mAuth
+                                .startActivityForSignInWithProvider(/* activity= */ AuthActivity.this, provider.build())
+                                .addOnSuccessListener(
+                                        new OnSuccessListener<AuthResult>() {
+                                            @Override
+                                            public void onSuccess(AuthResult authResult) {
+                                                openNextActivity();
+                                            }
+                                        })
+                                .addOnFailureListener(
+                                        new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Toast.makeText(AuthActivity.this,""+e.getMessage(),Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+
+                    }
+
+                }
+            }
+        });
+
+    }
+
+    private void openNextActivity() {
+        Intent intent = new Intent(AuthActivity.this,HomeActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+    }
+}
